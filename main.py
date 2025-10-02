3f5d8b6d3145ead989b81ddf3f35f67f4489160f
Dmanchil <Scheulov.dima@mail.ru>
2025-10-02 22:34:21 +0500

    не глобал обновление
    увеличена эффективность
    добавлено исключение для воскресенья

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import camelot
 import openpyxl 
 import telebot
-import requests
 from datetime import datetime, timedelta 
 from telebot import types
 import os
 import urllib.request
 
-token='8347380655:AAE56FocrVCTzAY39vc4QOo9Oz0IsZttcBw'
+token='8269738099:AAETqsa8WwNzhBfVH2zLay7_svsH_DLQDTc'
+#8347380655:AAE56FocrVCTzAY39vc4QOo9Oz0IsZttcBw ориг
+#8269738099:AAETqsa8WwNzhBfVH2zLay7_svsH_DLQDTc тест
 bot=telebot.TeleBot(token)
 
 
@@ -36,327 +37,98 @@ def aaa(message):
 
 
         #Добавляем кнопки
-        kb1b1= telebot.types.InlineKeyboardButton(text=f"{need_day(1)}",callback_data=f"{need_day(1)}")
-        kb1b2= telebot.types.InlineKeyboardButton(text=f"{need_day(2)}",callback_data=f"{need_day(2)}")
-        kb1b3= telebot.types.InlineKeyboardButton(text=f"{need_day(3)}",callback_data=f"{need_day(3)}")
-        kb2b1= telebot.types.InlineKeyboardButton(text=f"{need_day(-1)}",callback_data=f"{need_day(-1)}")
-        kb2b2= telebot.types.InlineKeyboardButton(text=f"{need_day(-2)}",callback_data=f"{need_day(-2)}")
-        kb2b3= telebot.types.InlineKeyboardButton(text=f"{need_day(-3)}",callback_data=f"{need_day(-3)}")
-        kb3b1= telebot.types.InlineKeyboardButton(text=f"<--",callback_data=f">")
-        kb3b2= telebot.types.InlineKeyboardButton(text=f"Сегодня",callback_data=f"now")
-        kb3b3= telebot.types.InlineKeyboardButton(text=f"-->",callback_data=f"<")
+        
 
+
+
+        b1= telebot.types.InlineKeyboardButton(text=f"<--",callback_data=f">")
+        b2= telebot.types.InlineKeyboardButton(text=f"Сегодня",callback_data=f"{datetime_full(0)}")
+        b3= telebot.types.InlineKeyboardButton(text=f"-->",callback_data=f"<")
+
+        b6= telebot.types.InlineKeyboardButton(text=f"{need_day(1)}",callback_data=f"{datetime_full(1)}")
+        b7= telebot.types.InlineKeyboardButton(text=f"{need_day(2)}",callback_data=f"{datetime_full(2)}")
+        b8= telebot.types.InlineKeyboardButton(text=f"{need_day(3)}",callback_data=f"{datetime_full(3)}")
+        b9= telebot.types.InlineKeyboardButton(text=f"{need_day(-1)}",callback_data=f"{datetime_full(-1)}")
+        b10= telebot.types.InlineKeyboardButton(text=f"{need_day(-2)}",callback_data=f"{datetime_full(-2)}")
+        b11= telebot.types.InlineKeyboardButton(text=f"{need_day(-3)}",callback_data=f"{datetime_full(-3)}")
+        
         #Вставляем в клавиатуру
-        kb1.add(kb1b1,kb1b2,kb1b3,kb2b1,kb2b2,kb2b3,kb3b1,kb3b2,kb3b3)
+        kb1.add(b6,b7,b8,b9,b10,b11,b1,b2,b3)
 
         bot.send_message(message.chat.id, "Привет", reply_markup=kb1) #Выводим клавиатуру
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-
-    if call.data == "now":
-        url=f"https://gtnu.ru/wp-content/uploads/rasp/{datetime_full(0)}.pdf"
-        urllib.request.urlretrieve(url, f'{datetime_full(0)}.pdf')
-        
-
-
-
-        desting = f'{datetime_full(0)}.xlsx'
-
-        a = camelot.read_pdf(f'{datetime_full(0)}.pdf')
-        os.remove(f'{datetime_full(0)}.pdf')
-        a[1].df.to_excel(desting)
-        
-        #открываем фаил exel
-        fff=openpyxl.load_workbook(desting) 
-        os.remove(f'{datetime_full(0)}.xlsx') 
-        f=fff.active
-        g=[]
-
-        #Ищем сколько всего пар
-        for i in range(f.max_row-2):
-            if f.cell(row=f.max_row-i,column=5).value is None:
-                exit
-            else:
-                g.append([int(f.cell(row=f.max_row-i,column=5).value),[f.max_row-i,5]])
-        g = g[::-1]    
-        #Состовляем сообщение
-        mes=""
-        for i in range(len(g)):
-            if None is f.cell(row=g[i][1][0],column=g[i][1][1]+1).value:
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}\n"
-
-            else:
-
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[0]}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()[len(f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines())-1]}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[1]}\n"
-        bot.send_message(call.from_user.id, f"Расписание на {need_day(-3)}\n"+mes)#Отправляем
-    elif call.data == f"<":
-
-
+    if call.data == f"<":
 
         bot.send_message(call.from_user.id, "Пока не работает, терпи.")
 
     elif call.data == f">":
     
-
-
         bot.send_message(call.from_user.id, "Пока не работает, терпи.")
 
-    elif call.data == f"{need_day(1)}":
-        url=f"https://gtnu.ru/wp-content/uploads/rasp/{datetime_full(1)}.pdf"
-        urllib.request.urlretrieve(url, f'{datetime_full(1)}.pdf')
-        
+    else:
+        send_mes(call)
 
 
-
-        desting = f'{datetime_full(1)}.xlsx'
-
-        a = camelot.read_pdf(f'{datetime_full(1)}.pdf')
-        os.remove(f'{datetime_full(1)}.pdf')
-        a[1].df.to_excel(desting)
-        
-        #открываем фаил exel
-        fff=openpyxl.load_workbook(desting) 
-        os.remove(f'{datetime_full(1)}.xlsx') 
-        f=fff.active
-        g=[]
-
-        #Ищем сколько всего пар
-        for i in range(f.max_row-2):
-            if f.cell(row=f.max_row-i,column=5).value is None:
-                exit
-            else:
-                g.append([int(f.cell(row=f.max_row-i,column=5).value),[f.max_row-i,5]])
-        g = g[::-1]    
-
-        #Состовляем сообщение
-        mes=""
-        for i in range(len(g)):
-            if None is f.cell(row=g[i][1][0],column=g[i][1][1]+1).value:
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}\n"
-
-            else:
-
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[0]}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()[len(f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines())-1]}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[1]}\n"
-        bot.send_message(call.from_user.id, f"Расписание на {need_day(1)}\n"+mes)#Отправляем
-
-
-
-
-    elif call.data == f"{need_day(2)}":
-        url=f"https://gtnu.ru/wp-content/uploads/rasp/{datetime_full(2)}.pdf"
-        urllib.request.urlretrieve(url, f'{datetime_full(2)}.pdf')
-        
-
-
-
-        desting = f'{datetime_full(2)}.xlsx'
-
-        a = camelot.read_pdf(f'{datetime_full(2)}.pdf')
-        os.remove(f'{datetime_full(2)}.pdf')
-        a[1].df.to_excel(desting)
-        
-        #открываем фаил exel
-        fff=openpyxl.load_workbook(desting) 
-        os.remove(f'{datetime_full(2)}.xlsx') 
-        f=fff.active
-        g=[]
-
-        #Ищем сколько всего пар
-        for i in range(f.max_row-2):
-            if f.cell(row=f.max_row-i,column=5).value is None:
-                exit
-            else:
-                g.append([int(f.cell(row=f.max_row-i,column=5).value),[f.max_row-i,5]])
-        g = g[::-1]    
-        #Состовляем сообщение
-        mes=""
-        for i in range(len(g)):
-            if None is f.cell(row=g[i][1][0],column=g[i][1][1]+1).value:
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}\n"
-
-            else:
-
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[0]}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()[len(f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines())-1]}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[1]}\n"
-        bot.send_message(call.from_user.id, f"Расписание на {need_day(2)}\n"+mes)#Отправляем
-
-
-
-
-    elif call.data == f"{need_day(3)}":
-        url=f"https://gtnu.ru/wp-content/uploads/rasp/{datetime_full(3)}.pdf"
-        urllib.request.urlretrieve(url, f'{datetime_full(3)}.pdf')
-        
-
-
-
-        desting = f'{datetime_full(3)}.xlsx'
-
-        a = camelot.read_pdf(f'{datetime_full(3)}.pdf')
-        os.remove(f'{datetime_full(3)}.pdf')
-        a[1].df.to_excel(desting)
-        
-        #открываем фаил exel
-        fff=openpyxl.load_workbook(desting) 
-        os.remove(f'{datetime_full(3)}.xlsx') 
-        f=fff.active
-        g=[]
-
-        #Ищем сколько всего пар
-        for i in range(f.max_row-2):
-            if f.cell(row=f.max_row-i,column=5).value is None:
-                exit
-            else:
-                g.append([int(f.cell(row=f.max_row-i,column=5).value),[f.max_row-i,5]])
-        g = g[::-1]    
-        #Состовляем сообщение
-        mes=""
-        for i in range(len(g)):
-            if None is f.cell(row=g[i][1][0],column=g[i][1][1]+1).value:
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}\n"
-
-            else:
-
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[0]}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()[len(f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines())-1]}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[1]}\n"
-        bot.send_message(call.from_user.id, f"Расписание на {need_day(3)}\n"+mes)#Отправляем
-
-
-
-
-
-    elif call.data == f"{need_day(-1)}":
-        url=f"https://gtnu.ru/wp-content/uploads/rasp/{datetime_full(-1)}.pdf"
-        urllib.request.urlretrieve(url, f'{datetime_full(-1)}.pdf')
-        
-
-
-
-        desting = f'{datetime_full(-1)}.xlsx'
-
-        a = camelot.read_pdf(f'{datetime_full(-1)}.pdf')
-        os.remove(f'{datetime_full(-1)}.pdf')
-        a[1].df.to_excel(desting)
-        
-        #открываем фаил exel
-        fff=openpyxl.load_workbook(desting) 
-        os.remove(f'{datetime_full(-1)}.xlsx') 
-        f=fff.active
-        g=[]
-
-        #Ищем сколько всего пар
-        for i in range(f.max_row-2):
-            if f.cell(row=f.max_row-i,column=5).value is None:
-                exit
-            else:
-                g.append([int(f.cell(row=f.max_row-i,column=5).value),[f.max_row-i,5]])
-        g = g[::-1]    
-        #Состовляем сообщение
-        mes=""
-        for i in range(len(g)):
-            if None is f.cell(row=g[i][1][0],column=g[i][1][1]+1).value:
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}\n"
-
-            else:
-
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[0]}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()[len(f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines())-1]}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[1]}\n"
-        bot.send_message(call.from_user.id, f"Расписание на {need_day(-1)}\n"+mes)#Отправляем
-
-
-
-
-
-
-
-
-
-
-
-
-    elif call.data == f"{need_day(-2)}":
-        url=f"https://gtnu.ru/wp-content/uploads/rasp/{datetime_full(-2)}.pdf"
-        urllib.request.urlretrieve(url, f'{datetime_full(-2)}.pdf')
-        
-
-
-
-        desting = f'{datetime_full(-2)}.xlsx'
-
-        a = camelot.read_pdf(f'{datetime_full(-2)}.pdf')
-        os.remove(f'{datetime_full(-2)}.pdf')
-        a[1].df.to_excel(desting)
-        
-        #открываем фаил exel
-        fff=openpyxl.load_workbook(desting) 
-        os.remove(f'{datetime_full(-2)}.xlsx') 
-        f=fff.active
-        g=[]
-
-        #Ищем сколько всего пар
-        for i in range(f.max_row-2):
-            if f.cell(row=f.max_row-i,column=5).value is None:
-                exit
-            else:
-                g.append([int(f.cell(row=f.max_row-i,column=5).value),[f.max_row-i,5]])
-        g = g[::-1]    
-        #Состовляем сообщение
-        mes=""
-        for i in range(len(g)):
-            if None is f.cell(row=g[i][1][0],column=g[i][1][1]+1).value:
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}\n"
-
-            else:
-
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[0]}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()[len(f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines())-1]}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[1]}\n"
-        bot.send_message(call.from_user.id, f"Расписание на {need_day(-2)}\n"+mes)#Отправляем
-
-
-
-
-
-
-
-
-
-
-    elif call.data == f"{need_day(-3)}":
+#elif call.data == f"{need_day(-3)}":
     
 
-        url=f"https://gtnu.ru/wp-content/uploads/rasp/{datetime_full(-3)}.pdf"
-        urllib.request.urlretrieve(url, f'{datetime_full(-3)}.pdf')
+def send_mes(call):
+    url=f"https://gtnu.ru/wp-content/uploads/rasp/{call.data}.pdf"
+    pdf=f'{call.data}.pdf'
+    try:
+        urllib.request.urlretrieve(url, pdf)
+    except urllib.error.HTTPError as err:
+
+        bot.send_message(call.from_user.id, f"Расписания нет. Отдыхай, разрешаю👍")
+        return None
+
         
+    
+
+    desting = f'{call.data}.xlsx'
+
+    a = camelot.read_pdf(pdf)
+    os.remove(pdf)
+    a[1].df.to_excel(desting)
+    
+    #открываем фаил exel
+    fff=openpyxl.load_workbook(desting) 
+    os.remove(f'{call.data}.xlsx') 
+    f=fff.active
+
+    #Ищем сколько всего пар
+    g=[]
+    for i in range(f.max_row-2):
+        if f.cell(row=f.max_row-i,column=5).value is None:
+            exit
+        else:
+            g.append([int(f.cell(row=f.max_row-i,column=5).value),[f.max_row-i,5]])
+    g = g[::-1]    
+    #Состовляем сообщение
+    mes=""
+    for i in range(len(g)):
+        if None is f.cell(row=g[i][1][0],column=g[i][1][1]+1).value:
+            break
+            #mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n        ———\n"
+
+
+        elif len(f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()) == 3:
+
+            mes = mes + f"\n{g[i][0]}       {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[0]}  —  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()[len(f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines())-1]}\n          {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()[1]}  —  {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[1]}\n"
+
+        else:#              1                                 08:30 - 09:55                                             Тех.мех.                                                        134                                                                                                                                                                 Лекции                                                                                                                                                                                      
+
+            mes = mes + f"\n{g[i][0]}       {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[0]}  —  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()[len(f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines())-1]}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[1]}\n"
+    
+
+    bot.send_message(call.from_user.id, f"Расписание на  {call.data[4:6:]}.{call.data[6:8:]}\n" + mes)#Отправляем
+    
 
 
 
-        desting = f'{datetime_full(-3)}.xlsx'
-
-        a = camelot.read_pdf(f'{datetime_full(-3)}.pdf')
-        os.remove(f'{datetime_full(-3)}.pdf')
-        a[1].df.to_excel(desting)
-        
-        #открываем фаил exel
-        fff=openpyxl.load_workbook(desting) 
-        os.remove(f'{datetime_full(-3)}.xlsx') 
-        f=fff.active
-        g=[]
-
-        #Ищем сколько всего пар
-        for i in range(f.max_row-2):
-            if f.cell(row=f.max_row-i,column=5).value is None:
-                exit
-            else:
-                g.append([int(f.cell(row=f.max_row-i,column=5).value),[f.max_row-i,5]])
-        g = g[::-1]    
-        #Состовляем сообщение
-        mes=""
-        for i in range(len(g)):
-            if None is f.cell(row=g[i][1][0],column=g[i][1][1]+1).value:
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value}\n"
-
-            else:
-
-                mes = mes + f"\n{g[i][0]}          {f.cell(row=g[i][1][0],column=2).value}\n    {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[0]}  -  {f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines()[len(f.cell(row=g[i][1][0]+1,column=g[i][1][1]+1).value.splitlines())-1]}\n          {f.cell(row=g[i][1][0],column=g[i][1][1]+1).value.splitlines()[1]}\n"
-        bot.send_message(call.from_user.id, f"Расписание на {need_day(-3)}\n"+mes)#Отправляем
 
 
 
